981c6270f29e1638afe9e0f5a1465de21264404c
hakyung <hakyung.dev@gmail.com>
2020-06-02 00:08:41 +0900

    Implement 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../layouts/index';
+import Not from '../images/notFound.png';
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <div className="section">
+        <div className="content not">
+          <h1 className="title">Page Not Found</h1>
+          <Link className="link" to="/">
+            &#8640; Head Home
+          </Link>
+          <div className="back-text">404</div>
+          <div className="float-container">
+            <img className="img" src={Not} alt="404" />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFound;
