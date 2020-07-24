b915b271a3f93caf85c6574e5fff454755c809b7
hakyung <hakyung.dev@gmail.com>
2020-07-25 00:04:53 +0900

    Add prismTheme.scss to load prism theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from './src/store/ThemeContext';
 
+require('./src/styles/themes/prismTheme.scss');
+
 export const wrapRootElement = ({ element }) => {
   return <ThemeProvider>{element}</ThemeProvider>;
 };
