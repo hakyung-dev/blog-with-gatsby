d92926babad49855a1c23907adfe96abcda58a15
hakyung <hakyung.dev@gmail.com>
2020-08-21 00:54:08 +0900

    Update config, dependencies, images for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,30 @@ module.exports = {
   siteMetadata: {
     title: config.siteTitle,
     author: config.author,
+    siteUrl: config.siteUrl,
   },
   plugins: [
     'gatsby-plugin-sass',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: ['/blog'], disallow: ['/about'] }],
+      },
+    },
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'contents',
         path: `${__dirname}/contents`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: `${__dirname}/static`,
       },
     },
     'gatsby-plugin-sharp',
